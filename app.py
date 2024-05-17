47e6b0de0290cd63e54debce3a58c2997d307f20
JeanRauta <j34nwr@gmail.com>
2024-05-17 15:46:42 -0300

    teste2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/')
 def index():
-    return 'o otavio é gay'
+    return render_template('index.html')
 
 @app.get('/teste/<userName>')
 def user(userName):
