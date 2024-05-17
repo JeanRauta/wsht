b9e98312dee5fe52307d058245d632246433ade9
JeanRauta <j34nwr@gmail.com>
2024-05-17 16:00:24 -0300

    teste

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask , render_template
+from chord_extractor.extractors import Chordino
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    chordino = Chordino()
+    acordes = chordino.extract(caminho_da_musica)
+    caminho_da_musica = "./static/teste.wav"
+    for acorde in acordes:
+        print(acorde.chord)
+
     return render_template('index.html')
 
 @app.get('/teste/<userName>')
@@ -12,3 +19,4 @@ def user(userName):
 
 if __name__ == '__main__':
     app.run()
+
