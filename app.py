33357dc5395c8c225edb7c13e8211ff1dbe0cc50
JeanRauta <j34nwr@gmail.com>
2024-05-25 13:56:47 -0300

    teste

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask , render_template
-from chord_extractor.extractors import Chordino
+import demucs.api
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    chordino = Chordino()
-    acordes = chordino.extract(caminho_da_musica)
-    caminho_da_musica = "./static/teste.wav"
-    for acorde in acordes:
-        print(acorde.chord)
-
-    return render_template('index.html')
+    teste = 'jean bonito'
+    return render_template('index.html', texto=teste)
 
 @app.get('/teste/<userName>')
 def user(userName):
