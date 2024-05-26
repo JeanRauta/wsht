524977a14da2a14f12091e0c652594bfc3902570
JeanRauta <j34nwr@gmail.com>
2024-05-25 21:44:47 -0300

    teste

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 import demucs.api
+from gunicorn.app.base import BaseApplication
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ separator = demucs.api.Separator()
 
 @app.route('/')
 def process_audio():
-    audio_file_path = './audio.wav' 
+    audio_file_path = './audio.wav'
 
     if not os.path.exists(audio_file_path):
         return jsonify({'message': 'Arquivo de áudio não encontrado'}), 404
@@ -20,5 +21,25 @@ def process_audio():
 
     return jsonify({'message': 'Processamento concluído'}), 200
 
+class GunicornApp(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(GunicornApp, self).__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', '5000'),
+        'workers': 1,
+        'timeout': 120,  # Ajuste o valor de timeout conforme necessário
+    }
+    GunicornApp(app, options).run()
